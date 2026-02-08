ad1a015e571ae0bb682badf4593f3e03ae00310b
Isabel Zheng <iz388@usf.edu>
2026-02-07 22:18:27 -0500

    Changed customer's email

--- cc_3.js
+++ cc_3.js
@@ -1,12 +1,19 @@
 let customers = ["Chad", "Tanner", "Ryan"]; 
-
+let email = ["Chad@vsc.com", "Tanner@vsc.com", "Ryan@vsc.com"]; 
+let purchases = [
+    ["Nike", "Air Jordans", "ASICS"], // Chad's purchases
+    ["Adidas", "Puma", "Under Armor"], // Tanner's purchases
+    ["Converse", "Vans", "New Balance"] // Ryan's purchases 
+]
 
 
 let newCustomer = "Parker"; 
 customers.push(newCustomer); 
 customers.shift(); 
 
+email[1]= "Tanner4@vsc.com"; 
 
-console.log(customers[0]); 
+
+console.log(email[1]); 
 
    
